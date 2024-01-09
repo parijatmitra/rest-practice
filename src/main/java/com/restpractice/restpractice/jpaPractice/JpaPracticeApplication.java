bdaa102b9b82d30ebbd39c602de4e6651d855c9f
“parijatmitra” <parijatmitra20@gmail.com>
2024-01-10 04:20:34 +0530

    jpa spring boot application file creation

---
+++ src/main/java/com/restpractice/restpractice/jpaPractice/JpaPracticeApplication.java
@@ -0,0 +1,11 @@
+package com.restpractice.restpractice.jpaPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPracticeApplication.class, args);
+    }
+}
